7fd21025d1be365477679cc5380aaf4bd6f9d495
Christi Reid <christi.reid@weareadaptive.com>
2024-02-05 14:55:07 -0500

    initial setup

---
+++ src/main/java/Console/Menu.java
@@ -0,0 +1,46 @@
+package Console;
+
+import java.io.Console;
+import java.util.Scanner; // 1
+
+
+public class Menu {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        Console console = System.console();
+        System.out.println("===================================");
+        System.out.println("1. Login");
+        System.out.println("2. Quit ");
+        System.out.println("===================================");
+        System.out.println("Enter your option: ");
+        int option = userInput.nextInt();
+
+
+
+
+        switch(option)
+        {
+            case 1:
+                String password;
+                System.out.println("Enter username: ");
+                String username = userInput.nextLine();
+                System.out.println("Enter password: ");
+                if (console != null) {
+                    password = String.valueOf(console.readPassword("Enter password: "));
+                } else {
+                    password = userInput.nextLine();
+                }
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("You have entered an invalid option");
+                return;
+        }
+
+
+    }
+
+}
